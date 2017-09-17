ecf459ccb44bd9fe8100e683d7cc6204c9c4984d
Shota Tamura <junkboy0315@gmail.com>
2017-09-17 19:23:42 +0900

    classified AssetPair related things

--- src/kraken.py
+++ src/kraken.py
@@ -1,64 +1,57 @@
+import sys
 import krakenex
 
 API = krakenex.API(
     key='ylRM81xbtOMiRSV2scfP2G1aZi7k5IG4wMGYslNjZwXvFzmLqK8z2wk3',
     secret='7wcryvE8yj7ogJTB8nneQ4H3OEvszJRg1WAonaayEJTCxK10xni2Wu3368DrZvRmAPhvaCWItYCCWzXfhq42Sw=='
 )
-TARGET_CURRENCY = 'JPY'
+
 BALANCE = API.query_private('Balance')['result']
-ASSET_PAIRS = API.query_public('AssetPairs')['result']
+TARGET_CURRENCY = 'JPY'
+CURRENCIES = ['USD', 'EUR', 'JPY', 'CAD', 'GBP']
 
 my_balance = []
+query = set()
+
+class AssetPair():
+    """Handle asset pairs."""
+
+    VALID_PAIRS = set(API.query_public('AssetPairs')['result'].keys())
+
+    @classmethod
+    def get_pair_name(cls, origin, target):
+
+        # pairs contains following asset class don't contain first 'X' and inner 'Z'.
+        #   e.g. 'BCHXBT', 'DASHXBT'
+        # but others contains like as follows.
+        #   e.g. 'XXBTZJPY', 'XETHZEUR'
+        IRREGULAR_ASSET_CLASSES = ['BCH', 'DASH', 'EOS', 'GNO']
+
+        if origin in IRREGULAR_ASSET_CLASSES:
+            return origin + target
+        elif target in CURRENCIES:
+            return 'X{}Z{}'.format(origin, target)
+        else:
+            return 'X{}X{}'.format(origin, target)
+
+    @classmethod
+    def is_valid_pair(cls, origin, target):
+        if cls.get_pair_name(origin, target) in cls.VALID_PAIRS:
+            return True
+        return False
+
+    @classmethod
+    def is_valid_pair_name(cls, pair_name):
+        if pair_name in cls.VALID_PAIRS:
+            return True
+        return False
 
 class Asset():
-    """ An unit reprsent each cryptocurrencies in the bank
-
-    :param name: a name of cryptocurrency
-    :type name: str
-    :param amount: an amount of cryptocurrency
-    :type secret: float
-    :returns: None
-
-    """
     def __init__(self, name, amount):
         self.name = name
         self.amount = float(amount)
-        self.amount_as_CURRENCY = None
+        self.amount_as_money = None
         self.calc_via_XBT_needed = None
-
-def get_pair_name(origin, target):
-    '''
-    get the right pair name for the API query.
-
-    if the pair is not contained by the Kraken's pair list,
-    return False, otherwise return pair string like 'XXBTZJPY'
-    '''
-
-    # pairs of these asset class doesn't contain first 'X' and inner 'Z'.
-    #   e.g. 'BCHXBT', 'DASHXBT'
-    # but others contains like as follows.
-    #   e.g. 'XXBTZJPY', 'XETHZEUR'
-    IRREGULAR_ASSET_CLASSES = ['BCH', 'DASH', 'EOS', 'GNO']
-    CURRENCIES = ['USD', 'EUR', 'JPY', 'CAD', 'GBP']
-
-    pair_name = ''
-
-    if origin in IRREGULAR_ASSET_CLASSES:
-        pair_name = origin + target
-    elif target in CURRENCIES:
-        pair_name = 'X{}Z{}'.format(origin, target)
-    else:
-        pair_name ='X{}X{}'.format(origin, target)
-
-    if is_right_pair_name(pair_name) is False:
-        return False
-    
-    return pair_name
-
-def is_right_pair_name(pair_name):
-    if pair_name not in ASSET_PAIRS.keys():
-        return False
-    return True
 
 # store assets info to the array
 for name, amount in BALANCE.items():
@@ -69,39 +62,44 @@ for name, amount in BALANCE.items():
 
     my_balance.append(Asset(name, amount))
 
-# judge calculation via XBT is needed
-for asset_class in my_balance:
-    if get_pair_name(asset_class.name, TARGET_CURRENCY) is False:
-        asset_class.calc_via_XBT_needed = True
-
-query = set()
-
-for asset_class in my_balance:
-    if asset_class.calc_via_XBT_needed:
-        query.add(get_pair_name(asset_class.name, 'XBT'))
-        query.add(get_pair_name('XBT', TARGET_CURRENCY))
+# calc asset pairs need to fetch
+for asset in my_balance:
+    if AssetPair.is_valid_pair(asset.name, TARGET_CURRENCY):
+        # get the amount of money directly
+        query.add(AssetPair.get_pair_name(asset.name, TARGET_CURRENCY))
     else:
-        query.add(get_pair_name(asset_class.name, TARGET_CURRENCY))
+        # get the amount money via XBT
+        asset.calc_via_XBT_needed = True
+        query.add(AssetPair.get_pair_name(asset.name, 'XBT'))
+        query.add(AssetPair.get_pair_name('XBT', TARGET_CURRENCY))
 
+# if the query contains asset pairs that are not valid,
+# display that information and exit tht program.
+if not query <= AssetPair.VALID_PAIRS:
+    sys.exit('The query contains invalid AssetPair: ' + str(query - AssetPair.VALID_PAIRS))
+
+# join the query and make a string => 'XXBTZJPY,BCHXBT'
 query = ','.join(query)
+
+# get the ticker data
 ticker = API.query_public('Ticker', {'pair': query})['result']
 
 
-for asset_class in my_balance:
-    if asset_class.calc_via_XBT_needed:
-        pair1 = get_pair_name(asset_class.name, 'XBT')
-        pair2 = get_pair_name('XBT', TARGET_CURRENCY)
-        
-        result = asset_class.amount * (
-            float(ticker[pair1]['c'][0])
-            * float(ticker[pair2]['c'][0])
+for asset in my_balance:
+    if asset.calc_via_XBT_needed:
+        pair1 = AssetPair.get_pair_name(asset.name, 'XBT')
+        pair2 = AssetPair.get_pair_name('XBT', TARGET_CURRENCY)
+
+        result = asset.amount * (
+            float(ticker[pair1]['c'][0]) *
+            float(ticker[pair2]['c'][0])
         )
 
-        asset_class.amount_as_CURRENCY = result
+        asset.amount_as_money = result
 
     else:
-        pair = get_pair_name(asset_class.name, TARGET_CURRENCY)
-        asset_class.amount_as_CURRENCY = asset_class.amount * float(ticker[pair]['c'][0])
+        pair = AssetPair.get_pair_name(asset.name, TARGET_CURRENCY)
+        asset.amount_as_money = asset.amount * float(ticker[pair]['c'][0])
 
 for asset in my_balance:
-    print(asset.name, '{:10.0f}'.format(asset.amount_as_CURRENCY))
+    print(asset.name, '{:10.0f}'.format(asset.amount_as_money))
--- src/kraken.spec.py
+++ src/kraken.spec.py
@@ -3,16 +3,19 @@ import kraken
 
 class MyTest(unittest.TestCase):
 
-    def test__get_pair_name(self):
-        self.assertEqual(kraken.get_pair_name('BCH', 'EUR'), 'BCHEUR')
-        self.assertEqual(kraken.get_pair_name('XBT', 'JPY'), 'XXBTZJPY')
-        self.assertEqual(kraken.get_pair_name('ETH', 'USD'), 'XETHZUSD')
-        self.assertEqual(kraken.get_pair_name('XRP', 'XBT'), 'XXRPXXBT')
+    def test_class_AssetPair(self):
+        self.assertEqual(kraken.AssetPair.is_valid_pair('XBT', 'JPY'), True)
+        self.assertEqual(kraken.AssetPair.is_valid_pair('XBT', 'XBT'), False)
+        self.assertEqual(kraken.AssetPair.is_valid_pair('XRP', 'JPY'), False)
+        
+        self.assertEqual(kraken.AssetPair.is_valid_pair_name('XXBTZJPY'), True)
+        self.assertEqual(kraken.AssetPair.is_valid_pair_name('XXBTXXBT'), False)
+        self.assertEqual(kraken.AssetPair.is_valid_pair_name('XXRPZJPY'), False)
 
-    def test__is_right_pair_name(self):
-        self.assertEqual(kraken.is_right_pair_name('XXBTZJPY'), True)
-        self.assertEqual(kraken.is_right_pair_name('XXBTXXBT'), False)
-        self.assertEqual(kraken.is_right_pair_name('XXRPZJPY'), False)
+        self.assertEqual(kraken.AssetPair.get_pair_name('BCH', 'EUR'), 'BCHEUR')
+        self.assertEqual(kraken.AssetPair.get_pair_name('XBT', 'JPY'), 'XXBTZJPY')
+        self.assertEqual(kraken.AssetPair.get_pair_name('ETH', 'USD'), 'XETHZUSD')
+        self.assertEqual(kraken.AssetPair.get_pair_name('XRP', 'XBT'), 'XXRPXXBT')
 
 if __name__ == "__main__":
     unittest.main()
