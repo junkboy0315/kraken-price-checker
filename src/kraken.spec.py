63f2820763fc8bd6c468002a66ac4369980e8f47
Shota Tamura <junkboy0315@gmail.com>
2017-09-19 21:44:14 +0900

    update test

--- src/kraken.spec.py
+++ src/kraken.spec.py
@@ -8,7 +8,7 @@ import kraken
 class MyTest(unittest.TestCase):
     def test_class_ApiHelper(self):
         # get_asset_pairs()
-        pairs = kraken.ApiHelper.get_asset_pairs()
+        pairs = kraken.ApiHelper.get_asset_pair_names()
         self.assertEqual({'XXBTZGBP', 'GNOXBT'} <= pairs, True)
 
         # get_ticker()
@@ -17,20 +17,15 @@ class MyTest(unittest.TestCase):
 
     def test_class_AssetPair(self):
         # is_valid_pair()
-        self.assertEqual(kraken.AssetPair.is_valid_pair('XBT', 'JPY'), True)
-        self.assertEqual(kraken.AssetPair.is_valid_pair('XBT', 'XBT'), False)
-        self.assertEqual(kraken.AssetPair.is_valid_pair('XRP', 'JPY'), False)
-
-        # is_valid_pair_name()
-        self.assertEqual(kraken.AssetPair.is_valid_pair_name('XXBTZJPY'), True)
-        self.assertEqual(kraken.AssetPair.is_valid_pair_name('XXBTXXBT'), False)
-        self.assertEqual(kraken.AssetPair.is_valid_pair_name('XXRPZJPY'), False)
+        self.assertEqual(kraken.AssetPair.is_valid('XBT', 'JPY'), True)
+        self.assertEqual(kraken.AssetPair.is_valid('XBT', 'XBT'), False)
+        self.assertEqual(kraken.AssetPair.is_valid('XRP', 'JPY'), False)
 
         # generate_pair_name()
-        self.assertEqual(kraken.AssetPair.generate_pair_name('BCH', 'EUR'), 'BCHEUR')
-        self.assertEqual(kraken.AssetPair.generate_pair_name('XBT', 'JPY'), 'XXBTZJPY')
-        self.assertEqual(kraken.AssetPair.generate_pair_name('ETH', 'USD'), 'XETHZUSD')
-        self.assertEqual(kraken.AssetPair.generate_pair_name('XRP', 'XBT'), 'XXRPXXBT')
+        self.assertEqual(kraken.AssetPair.generate_name('BCH', 'EUR'), 'BCHEUR')
+        self.assertEqual(kraken.AssetPair.generate_name('XBT', 'JPY'), 'XXBTZJPY')
+        self.assertEqual(kraken.AssetPair.generate_name('ETH', 'USD'), 'XETHZUSD')
+        self.assertEqual(kraken.AssetPair.generate_name('XRP', 'XBT'), 'XXRPXXBT')
 
 if __name__ == "__main__":
     unittest.main()
